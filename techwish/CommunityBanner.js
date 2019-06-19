9982d18df2d7e109e4e05dd4b3541675c73ba622
Gabriel Birke <gabriel.birke@wikimedia.de>
2019-06-19 17:24:42 +0100

    Keep banners inside siteNotice area
    Other than the fundraising or author campaign banners, the banner must
    stay inside the siteNotice container provided by CentralNotice.

--- techwish/CommunityBanner.js
+++ techwish/CommunityBanner.js
@@ -21,15 +21,12 @@ export class CommunityBanner {
 	createBanner() {
 		const $bannerContainer = $( '#WMDE-Banner-Container' );
 		$bannerContainer.html( this.bannerTemplate( {} ) );
-		$( 'body' ).prepend( $( '#centralNotice' ) );
-		this.addBannerSpace();
 		$( '#banner-container' ).show();
-		$( window ).resize( this.addBannerSpace );
+
 	}
 
 	removeBanner() {
 		$( '#banner-container' ).hide();
-		this.removeBannerSpace();
 	}
 
 	registerClickEvents() {
@@ -47,59 +44,4 @@ export class CommunityBanner {
 		} );
 	}
 
-	addBannerSpace() {
-		const bannerHeight = $( '#banner-container' ).height();
-		switch ( mw.config.get( 'skin' ) ) {
-			case 'vector': {
-				$( '#mw-panel' ).css( 'top', bannerHeight );
-				$( '#mw-head' ).css( 'top', bannerHeight );
-				break;
-			}
-			case 'monobook': {
-				const wrapper = $( '#globalWrapper' );
-				const banner = $( '#banner-container' );
-				wrapper.css( 'position', 'relative' );
-				banner.css( 'background', 'none' );
-				$( 'div#centralNotice > *' ).attr( 'style', 'margin-top: 0 !important' );
-				break;
-			}
-			case 'timeless': {
-				$( '#mw-header-container' ).css( {
-					position: 'relative'
-				} );
-				$( '#mw-header-hack' ).css( {
-					position: 'relative',
-					top: 0
-				} );
-				$( '#mw-content-container' ).css( 'margin-top', '0' );
-			}
-		}
-	}
-
-	removeBannerSpace() {
-		switch ( mw.config.get( 'skin' ) ) {
-			case 'vector': {
-				$( '#mw-panel' ).css( 'top', 0 );
-				$( '#mw-head' ).css( 'top', 0 );
-				$( '#mw-page-base' ).css( 'padding-top', 0 );
-				break;
-			}
-			case 'monobook': {
-				const wrapper = $( '#globalWrapper' );
-				wrapper.css( 'position', 'relative' );
-				wrapper.css( 'top', 0 );
-				break;
-			}
-			case 'timeless': {
-				$( '#mw-header-container' ).css( {
-					position: 'fixed'
-				} );
-				$( '#mw-header-hack' ).css( {
-					position: 'fixed',
-					top: '3.125em'
-				} );
-				$( '#mw-content-container' ).css( 'margin-top', '3.125em' );
-			}
-		}
-	}
 }
