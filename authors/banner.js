a51983e3586306ce69f882b7e6e7e94e8cf11922
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-10-28 18:22:39 +0100

    Implement Fall 2019 Author's banner

--- authors/banner.js
+++ authors/banner.js
@@ -3,7 +3,7 @@ require( './css/styles.pcss' );
 import { AuthorsBanner } from './AuthorsBanner';
 
 $( document ).ready( function () {
-	const bannerName = 'WMDE_neweditors_autumn_2018_lp1';
+	const bannerName = 'WMDE_neweditors_autumn_2019_lp1';
 	const bannerTemplate = require( './templates/banner.hbs' );
 
 	const authorsBanner = new AuthorsBanner( bannerName, bannerTemplate );
