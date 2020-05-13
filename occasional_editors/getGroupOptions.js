cf930ad927c884cf3d2ddc6727e63282006ed7d4
Abban Dunne <abban.dunne@wikimedia.de>
2020-05-13 17:06:22 +0200

    Update styles

--- occasional_editors/getGroupOptions.js
+++ occasional_editors/getGroupOptions.js
@@ -5,6 +5,9 @@ export const GROUP_TYPE_NONE = 2;
 const GROUP_TYPE_SUFFIX_A = 'cs';
 const GROUP_TYPE_SUFFIX_B = 'nt';
 
+const GROUP_TYPE_BANNER_TEXT_A = 'Danke, deine Bearbeitungen verbessern Wikipedia! Du bist nicht alleine: <b>Förderangebote</b> für Autorinnen und Autoren wie dich!';
+const GROUP_TYPE_BANNER_TEXT_B = 'Danke, deine Bearbeitungen verbessern Wikipedia! Du willst mehr: Spannende <b>Aufgaben</b> & persönliche <b>Unterstützung</b> genau für dich.';
+
 export function getGroupType( allowGroupA, allowGroupB ) {
 	const editCount = mw.config.get( 'wgUserEditCount' );
 	const daysRegistered = Math.floor( ( Date.now() - mw.config.get( 'wgUserRegistration' ) ) / ( 1000 * 3600 * 24 ) );
@@ -25,6 +28,18 @@ export function getGroupTypeSuffix( groupType ) {
 
 	if ( groupType === GROUP_TYPE_B ) {
 		return GROUP_TYPE_SUFFIX_B;
+	}
+
+	return '';
+}
+
+export function getGroupBannerText( groupType ) {
+	if ( groupType === GROUP_TYPE_A ) {
+		return GROUP_TYPE_BANNER_TEXT_A;
+	}
+
+	if ( groupType === GROUP_TYPE_B ) {
+		return GROUP_TYPE_BANNER_TEXT_B;
 	}
 
 	return '';
