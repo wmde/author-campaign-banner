ea4daa5fab0b048c8f3cba53a51e29b498bffb4b
Kai Nissen <kai.nissen@wikimedia.de>
2020-05-14 12:27:20 +0200

    fix punctuation

--- occasional_editors/getGroupOptions.js
+++ occasional_editors/getGroupOptions.js
@@ -5,8 +5,8 @@ export const GROUP_TYPE_NONE = 2;
 const GROUP_TYPE_SUFFIX_A = 'cs';
 const GROUP_TYPE_SUFFIX_B = 'nt';
 
-const GROUP_TYPE_BANNER_TEXT_A = 'Du bist nicht alleine: <b>Förderangebote</b> für Autorinnen und Autoren wie dich!';
-const GROUP_TYPE_BANNER_TEXT_B = 'Du willst mehr: Spannende <b>Aufgaben</b> & persönliche <b>Unterstützung</b> genau für dich.';
+const GROUP_TYPE_BANNER_TEXT_A = 'Du bist nicht alleine: <b>Förderangebote</b> für Autorinnen und Autoren wie dich.';
+const GROUP_TYPE_BANNER_TEXT_B = 'Du willst mehr? Spannende <b>Aufgaben</b> & persönliche <b>Unterstützung</b> genau für dich.';
 
 export function getGroupType( allowGroupA, allowGroupB ) {
 	const editCount = mw.config.get( 'wgUserEditCount' );
